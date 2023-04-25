302785a7d8056a006bc9b4cfa88f9c29dd4c7bc7
Ilya Ivanovsky <IlyaIvanovsky@outlook.com>
2023-04-25 17:19:00 +0800

    Create bomb2.py

---
+++ bomb2.py
@@ -0,0 +1,22 @@
+import Kitten 
+
+time = 60
+
+def Shot2():
+  Kitten.play_sound('gun_m24')
+  Kitten.show()
+  Kitten.move_to('坦克2')
+  Kitten.set_rotation(Kitten.value_of_object('坦克2', 'rotation'))
+  while True :
+    Kitten.move_forward(8)
+    if (Kitten.is_touched('Self', '人机') or Kitten.is_touched('Self', '坦克1') or Kitten.is_touched('Self', '炮弹1') or Kitten.is_touched('Self', '炮弹3') or Kitten.is_touched('Self', 'edge')) :
+      Kitten.hide()
+      break
+
+def when_get_signal(signal):
+  if signal == 'V1':
+    Kitten.stop_code('self_others')
+
+def when_get_signal(signal):
+  if signal == 'Restart':
+    Kitten.restart_project()
